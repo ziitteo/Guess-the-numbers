1c5b863b48b6dcf6155c9e008fc54f2bb6899699
Ziitteo <ziitteo@gamil.com>
2024-07-01 22:34:41 +0900

    숫자 맞추기 게임 완성

--- main.js
+++ main.js
@@ -9,29 +9,96 @@
 // 유저가 이미 입력한 숫자를 또 입력하면, 알려준다, 기회를 깍지 않는다
 
 
-let computerNumber = 0;
-let playButton = document.getElementById("play-button");
+let randomNumber = 0;
 let userInput = document.getElementById("user-input");
-let resultArea = document.getElementById("result-area");
+let playButton = document.getElementById("play-button");
+let resetButton = document.getElementById("reset-button");
+let resultImage = document.querySelector(".result-image");
+let resultText = document.querySelector(".result-text");
+let resultAnswer = document.querySelector(".result-answer");
+
+let chanceArea = document.getElementById("chance-area");
+let chances = 5;
+let gameOver = false;
+
+let history = [];
+
 
 const pickRandomNumber = () => {
-  computerNumber = Math.floor(Math.random() * 100) + 1;
-  return computerNumber;
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+  console.log(randomNumber);
+  return randomNumber;
+}
+
+const play = () => {
+  let userNumber = parseInt(userInput.value);
+
+  if (userNumber < 1 || userNumber > 100) {
+    resultImage.style.backgroundImage = "url(/assets/images/number.gif)";
+    resultText.textContent = "1~100 사이의 숫자를 입력해주세요";
+    return;
+  }
+
+  if (history.includes(userNumber)) {
+    resultImage.style.backgroundImage = "url(/assets/images/number.gif)";
+    resultText.textContent = "이미 입력했던 숫자입니다.";
+    return;
+  }
+
+  chances--;
+  chanceArea.textContent = `남은 찬스 : ${chances}번`;
+
+  if (chances < 1) {
+    gameOver = true;
+  }
+
+  if (gameOver) {
+    resultImage.style.backgroundImage = "url(/assets/images/gameover.gif)"
+    resultText.textContent = "게임오버!! 마셔라~ 마셔라~ 마셔라~"
+    resultAnswer.textContent = `정답은 ${randomNumber} 입니다.`
+    playButton.disabled = true;
+    return;
+  }
+
+  if (userNumber < randomNumber) {
+    resultImage.style.backgroundImage = "url(/assets/images/up.gif)";
+    resultText.textContent  = "Up!!"
+  } else if (userNumber > randomNumber) {
+    resultImage.style.backgroundImage = "url(/assets/images/down.gif)";
+    resultText.textContent  = "Down!!"
+  } else {
+    resultImage.style.backgroundImage = "url(/assets/images/clap.gif)";
+    resultText.textContent  = "맞춰버렸습니다~"
+    playButton.disabled = true;
+  }
+
+  history.push(userNumber);
+}
+
+const reset = () => {
+  // user Input 초기화
+  userInput.value = "";
+  // result Image 초기화
+  resultImage.style.backgroundImage = "url(/assets/images/start.gif)";
+  // result Text 초기화
+  resultText.textContent = "숫자를 맞춰보세요!"
+  // playButton 활성화
+  playButton.disabled = false;
+  // chance 초기화
+  chances = 5;
+  chanceArea.textContent = `남은 찬스 : ${chances}번`;
+  // gameOver 초기화
+  gameOver = false;
+  // history 초기화
+  history = [];
+  // 새로운 랜덤번호 지정
+  pickRandomNumber();
 }
 
 pickRandomNumber();
 
-const play = () => {
-  let userValue = userInput.value;
-
-  if (userValue < computerNumber) {
-    resultArea.textContent = "UP!!!";
-  } else if (userValue > computerNumber) {
-    resultArea.textContent = "DOWN!!!";
-  } else {
-    resultArea.textContent = "맞춰버렸다!!!";
-  }
-}
-
 playButton.addEventListener("click", play);
-
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", () => {
+  userInput.value = "";
+});
